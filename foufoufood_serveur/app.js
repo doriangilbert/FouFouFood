111a8edfebc93edd886fa54e742f43a76447553b
Dorian GILBERT <29594904+doriangilbert@users.noreply.github.com>
2024-10-28 00:53:48 -0400

    Mise à jour du gitignore de l'app

--- foufoufood_serveur/app.js
+++ foufoufood_serveur/app.js
@@ -310,17 +310,6 @@ app.get('/orders', authenticateToken, authorizeRoles('admin'), async (req, res) 
   }
 });
 
-// Route des commandes
-app.get('/orders', authenticateToken, authorizeRoles('admin'), async (req, res) => {
-  try {
-    const orders = await Order.find();
-    res.json(orders);
-  } catch (err) {
-    console.error(err);
-    res.status(500).json({ message: 'Server Error' });
-  }
-});
-
 // Consulter les détails d'une commande
 app.get('/orders/:id', authenticateToken, authorizeRoles('utilisateur', 'livreur', 'restaurant', 'admin'), async (req, res) => {
   try {
